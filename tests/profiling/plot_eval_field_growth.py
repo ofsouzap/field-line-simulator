31738c9bd17ae9dda3809c19d708da89456f7f65
Ofsouzap <ofsouzap@gmail.com>
2023-06-30 14:22:40 +0100

    Add profiling plotting scripts

---
+++ tests/profiling/plot_eval_field_growth.py
@@ -0,0 +1,110 @@
+from matplotlib import pyplot as plt
+from timeit import timeit
+import numpy as np
+from field import Field
+from point_source import PointSource
+
+
+clip_ranges = np.array([
+    [0, 100],
+    [0, 100]
+])
+
+strength_range = (0.0, 20.0)
+
+
+def single_test(test_count: int, source_count: int, pos_count: int, repeat_count: int = 10) -> np.ndarray:
+
+    results = np.zeros(shape=(test_count,), dtype=float)
+
+    for test_i in range(test_count):
+
+        # Generate random field
+
+        sources = np.random.rand(source_count, 3)
+
+        sources[:, 0] *= clip_ranges[0, 1] - clip_ranges[0, 0]
+        sources[:, 0] += clip_ranges[0, 0]
+        sources[:, 1] *= clip_ranges[1, 1] - clip_ranges[1, 0]
+        sources[:, 1] += clip_ranges[1, 0]
+        sources[:, 1] *= strength_range[1] - strength_range[0]
+        sources[:, 1] += strength_range[0]
+
+        field = Field()
+
+        for source_i in range(sources.shape[0]):
+            field.add_element(
+                PointSource(np.array([
+                    sources[source_i, 0],
+                    sources[source_i, 1]
+                ]),
+                sources[source_i, 2])
+            )
+
+        # Generate random inputs
+
+        poss = np.random.rand(pos_count, 2)
+        poss[:, 0] *= clip_ranges[0, 1] - clip_ranges[0, 0]
+        poss[:, 0] += clip_ranges[0, 0]
+        poss[:, 1] *= clip_ranges[1, 1] - clip_ranges[1, 0]
+        poss[:, 1] += clip_ranges[1, 0]
+
+        # Perform test
+
+        result = timeit(
+            lambda: field.evaluate(poss),
+            number=repeat_count
+        )
+
+        # Store result
+
+        results[test_i] = result
+
+    return results
+
+
+def main():
+
+    # Generate data
+
+    ns = np.arange(0, 100, 5, dtype=int)
+
+    results_list = []
+
+    for n in ns:
+
+        test_ress = single_test(
+            test_count=50,
+            source_count=n,
+            pos_count=20
+        )
+
+        results_list.append((n, test_ress))
+
+    x = np.array([n for (n, _) in results_list])
+
+    all_y = np.array([ress for (_, ress) in results_list])
+    y = np.mean(all_y, axis=1)
+    yerr = np.array([
+        y - np.min(all_y, axis=1),
+        np.max(all_y, axis=1) - y,
+    ])
+
+    # Plot data
+
+    _, ax = plt.subplots(1, 1)
+
+    ax.errorbar(
+        x=x,
+        y=y,
+        yerr=yerr
+    )
+
+    ax.set_ylabel("Field Evaluation Running Time")
+    ax.set_xlabel("Number of Sources")
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
