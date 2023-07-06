50f5e223ed8c27593a50d4949addb997e85744c4
Ofsouzap <ofsouzap@gmail.com>
2023-07-06 17:39:31 +0100

    Start controls bar

--- gui.py
+++ gui.py
@@ -3,6 +3,13 @@ from field import Field, List
 from point_source import PointSource
 import numpy as np
 from _debug_util import Timer
+from typing import Callable, Set, Any, List, Tuple
+from pathlib import Path
+from os.path import join as joinpath
+
+
+BLACK = (0, 0, 0, 255)
+WHITE = (255, 255, 255, 255)
 
 
 WINDOW_TITLE = "Field Line Simulator"
@@ -10,16 +17,137 @@ WINDOW_DEFAULT_WIDTH = 720
 WINDOW_DEFAULT_HEIGHT = 480
 
 
-class DiagramWindow(pyglet.window.Window):
+CONTROLS_ICONS_RESOURCES_DIR = "icons"
+"""The resources subdirectory containing images for the controls buttons' icons"""
+
+
+CONTROLS_IMAGE_LENGTH: int = 64
+CONTROLS_BAR_BUTTON_WIDTH: int = 100
+CONTROLS_BAR_HEIGHT: int = 100
+
+
+CONTROLS: List[Tuple[str, str]] = [
+    ("Save (Ctrl+S)", "save.png"),
+    ("Open (Ctrl+O)", "load.png"),
+    ("...", "placeholder.png"),
+    ("...", "placeholder.png"),
+    ("...", "placeholder.png"),
+    ("...", "placeholder.png"),
+    ("...", "placeholder.png"),
+]
+"""Details about each of the control options in the controls bar for the gui.
+
+Each tuple is:
+
+    label - the label for the control
+
+    image - the resource path for the control's icon relative to CONTROLS_ICONS_RESOURCES_DIR
+"""
+
+
+def controls_icon(image_path: str) -> pyglet.image.TextureRegion:
+    return pyglet.resource.image(joinpath(CONTROLS_ICONS_RESOURCES_DIR, image_path), atlas=False)
+
+
+class MainWindow(pyglet.window.Window):
 
     def __init__(self, width: int, height: int):
 
         super().__init__(width, height, WINDOW_TITLE)
 
-        self.batch = pyglet.graphics.Batch()
+        # Create graphics batches
+
+        self.diagram_batch = pyglet.graphics.Batch()
+        self.controls_batch = pyglet.graphics.Batch()
+
+        # Create members
 
         self.__lifetime = 0
         self.__field_line_lines = []
+
+        self.__controls_drawables: Set[Any] = set()
+
+        # Create controls panel
+
+        for i, (btn_label, btn_image) in enumerate(CONTROLS):
+
+            self.__create_controls_button(
+                btn_label=btn_label,
+                btn_image_path=btn_image,
+                on_press=lambda: print(f"Button \"{btn_label}\" pressed"),  # TODO - do actual functionalities
+                image_length=CONTROLS_IMAGE_LENGTH,
+                width=CONTROLS_BAR_BUTTON_WIDTH,
+                height=CONTROLS_BAR_HEIGHT,
+                index = i
+            )
+
+    def __create_controls_button(self,
+                                 btn_label: str,
+                                 btn_image_path: str,
+                                 on_press: Callable[[], None],
+                                 image_length: int,
+                                 width: int,
+                                 height: int,
+                                 index: int) -> None:
+        """Creates a button in the controls bar that is rendered when the window is drawn
+
+Parameters:
+
+    btn_label - the label to put for the button
+
+    btn_image_path - the path for the button's image relative to CONTROLS_ICONS_RESOURCES_DIR
+
+    on_press - the callback to call when the button is pressed
+
+    image_length - the image will be drawn as a square. This is the side length that this square should have
+
+    width - the width of the area that a button takes up. This should be greater than the button image's width
+
+    height - the height of the area that a button takes up. This should be greater than the button image's height
+
+    index - the index of the button. This is used to calculate the position of the button's image and label
+"""
+
+        # Calculate button positions
+
+        left = width * index
+        right = left + width
+        mid_x = left + (width // 2)
+
+        top = self.height
+        bottom = top - height
+
+        image_top = top
+        image_bottom = image_top - image_length
+        image_mid_y = (image_top + image_bottom) // 2
+
+        label_top = image_bottom
+        label_bottom = bottom
+        label_mid_y = (label_top + label_bottom) // 2
+
+        # Create label and image
+
+        label = pyglet.text.Label(
+            text=btn_label,
+            x=mid_x,
+            y=label_mid_y,
+            anchor_x="center",
+            anchor_y="center",
+            color=WHITE,
+            align="center",
+            multiline=True,
+            width=CONTROLS_BAR_BUTTON_WIDTH,
+            batch=self.controls_batch
+        )
+
+        self.__controls_drawables.add(label)
+
+        image = controls_icon(btn_image_path)
+        img_width_half = image.width // 2
+        img_height_half = image.height // 2
+        image_sprite = pyglet.sprite.Sprite(image, x=mid_x-img_width_half, y=image_mid_y-img_height_half, batch=self.controls_batch)
+
+        self.__controls_drawables.add(image_sprite)
 
     def draw_field(self,
                    field: Field) -> None:
@@ -89,7 +217,7 @@ class DiagramWindow(pyglet.window.Window):
             line = pyglet.shapes.Line(
                 prev[0], prev[1],
                 curr[0], curr[1],
-                batch=self.batch
+                batch=self.diagram_batch
             )
 
             self.__field_line_lines.append(line)
@@ -108,7 +236,9 @@ class DiagramWindow(pyglet.window.Window):
         """Clear screen and draw batch of shapes"""
 
         self.clear()
-        self.batch.draw()
+
+        self.diagram_batch.draw()
+        self.controls_batch.draw()
 
     def update(self, delta_time: float) -> None:
 
@@ -118,7 +248,7 @@ class DiagramWindow(pyglet.window.Window):
 def open_gui():
     """Create and open the main GUI. Blocks until the window is closed"""
 
-    window = DiagramWindow(WINDOW_DEFAULT_WIDTH, WINDOW_DEFAULT_HEIGHT)  # Create main window
+    window = MainWindow(WINDOW_DEFAULT_WIDTH, WINDOW_DEFAULT_HEIGHT)  # Create main window
 
     # TODO - below field-making is just an example, delete once proper gui with field editing is made
 
@@ -136,7 +266,7 @@ def open_gui():
 
     for i, s in enumerate(sources_dat):
 
-        source_shapes.append(pyglet.shapes.Circle(s[0], s[1], 3, batch=window.batch))
+        source_shapes.append(pyglet.shapes.Circle(s[0], s[1], 3, batch=window.diagram_batch))
 
         field.add_element(PointSource(np.array([s[0], s[1]]), s[2]))
 
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 #!/bin/env python3
 
 from gui import open_gui
+import pyglet
 
+
+# Set-up pyglet resources
+
+pyglet.resource.path = ["resources"]
+pyglet.resource.reindex()
+
+
+# Run main gui
 
 if __name__ == "__main__":
     open_gui()
